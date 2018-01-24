3fdaa05ec76a65145ed860075aba826648b9f05c
heinhtetps <heinhtet.pyisoe@gmail.com>
2018-01-23 22:49:43 -0500

    add logger and dotenv

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+
+//make the app
 const app = express();
 
 app.get('/', (req, res) => res.end('hey what is good?'));
-
 app.listen(3000, () => console.log('i am listening on port 3000'));
+
+//server logs
+app.use(morgan('combined'));
