171007ce604e1834f466c9282f3b344b3e62db66
heinhtetPS <heinhtet.pyisoe@gmail.com>
2018-01-29 18:40:46 -0500

    hello successfully routed, pug rendering, css inserted

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const morgan = require('morgan');
 //make the app
 const app = express();
 
-app.get('/', (req, res) => res.end('hello world'));
+app.get('/', (req, res) => {res.render('hello')});
 app.listen(3000, () => console.log('i am listening on port 3000'));
 
 //server logs
 app.use(morgan('combined'));
+app.set('views', 'views');
+app.set('view engine', 'pug');
