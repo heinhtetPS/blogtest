ccaac28073894f65bf35b96ce907cc9bfcbd2d1e
heinhtetPS <heinhtet.pyisoe@gmail.com>
2018-01-29 18:57:30 -0500

    resolved css path, css is now working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 //make the app
 const app = express();
@@ -10,5 +11,9 @@ app.listen(3000, () => console.log('i am listening on port 3000'));
 
 //server logs
 app.use(morgan('combined'));
+// SERVING STATIC FILES
+app.use(express.static(path.join(__dirname, 'styles')));
+
+
 app.set('views', 'views');
 app.set('view engine', 'pug');
